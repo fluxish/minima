bd422958f51dbe6a04fc7238b6fc47c7633a4a2b
Luigi Marco Simonetti <luigimarco@gmail.com>
2011-08-11 14:56:03 +0200

    added first draft of Upload class library

---
+++ system/library/Upload.php
@@ -0,0 +1,24 @@
+<?php  if (!defined('ROOT')) exit('No direct script access allowed');
+
+/**
+ * Kaili Upload Class
+ *
+ * Class to manage uploading of files to the server.
+ *
+ * @package		Kaili
+ * @subpackage	Library
+ * @category	Library
+ * @author		Luigi Marco Simonetti
+ */
+
+class Upload
+{
+
+    function __construct()
+    {
+        
+    }
+}
+
+/* End of file Upload.php */
+/* Location: ./system/library/Upload.php */
