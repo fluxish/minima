f7df9d8777da31f56279b7b67567dbd48d49a9f5
Luigi Marco Simonetti <luigimarco@gmail.com>
2011-08-23 20:46:51 +0200

    refactoring of the code

--- system/kaili/Loader.php
+++ system/kaili/Loader.php
@@ -1,4 +1,7 @@
-<?php  if ( ! defined('ROOT')) exit('No direct script access allowed');
+<?php
+
+if(!defined('ROOT'))
+    exit('No direct script access allowed');
 
 namespace Kaili;
 
@@ -9,16 +12,16 @@ namespace Kaili;
  *
  * @package	Kaili
  */
- 
 class Loader
 {
+
     /**
      * Singleton instance of Loader
      *
      * @var Loader
-     */   
+     */
     private static $_instance = null;
-    
+
     /**
      * Returns the singleton instance of the loader
      *
@@ -26,55 +29,54 @@ class Loader
      */
     public static function get_instance()
     {
-        if(self::$_instance == null){
+        if(self::$_instance == null) {
             self::$_instance = new Loader();
         }
         return self::$_instance;
     }
-    
-    /** 
+
+    /**
      * Autoload any classes that are required
      *
      * @param string $className the name of required class
      */
-    public static function __autoload($class) 
+    public static function __autoload($class)
     {
         // create the path from the name of the class
         $class = str_replace('\\', DS, $class);
-        
-	    if(file_exists(SYSTEM.DS.$class.EXT)) {
-		    require_once(SYSTEM.DS.'library'.DS.$path.EXT);
-	    }
+
+        if(file_exists(SYSTEM.DS.$class.EXT)) {
+            require_once(SYSTEM.DS.'library'.DS.$path.EXT);
+        }
         else if(file_exists(APPLICATION.DS.'controllers'.DS.$className.EXT)) {
-		    require_once(APPLICATION.DS.'controllers'.DS.$className.EXT);
-	    } 
-	    else if(file_exists(APPLICATION.DS.'models'.DS.$className.EXT)) {
-		    require_once(APPLICATION.DS.'models'.DS.$className.EXT);
-	    } 
+            require_once(APPLICATION.DS.'controllers'.DS.$className.EXT);
+        }
+        else if(file_exists(APPLICATION.DS.'models'.DS.$className.EXT)) {
+            require_once(APPLICATION.DS.'models'.DS.$className.EXT);
+        }
     }
-    
-    
+
     /**
      * Array of loaded libraries
      *
      * @var array
      */
     private $_loaded_libraries = array();
-    
+
     /**
      * Instance of Config class
      *
      * @var Config
      */
     private $_config;
-    
+
     /**
      * Array of default loaded libraries
      *
      * @var array
      */
     private $_internal_autoload = array('input', 'output');
-    
+
     /**
      * Create the Loader
      */
@@ -83,7 +85,7 @@ class Loader
         spl_autoload_register('Loader::__autoload');
         $this->_config = $this->library('config');
     }
-    
+
     /**
      * Load an helper
      * @param string $helper the unique name of the helper 
@@ -92,20 +94,20 @@ class Loader
     {
         include_once(SYSTEM.DS.'helpers'.DS.$helper.'.helper.php');
     }
-    
+
     /**
      * Load a class from the framework's library
      * @param string $lib the name of the class
      */
     public function library($lib)
     {
-        if(!array_key_exists($lib, $this->_loaded_libraries)){
+        if(!array_key_exists($lib, $this->_loaded_libraries)) {
             $libName = ucwords($lib);
             $this->_loaded_libraries[$lib] = new $libName();
         }
         return $this->_loaded_libraries[$lib];
     }
-    
+
     /**
      * Load an MVC controller
      * @param string $controller the name of the controller (for convention,
@@ -117,85 +119,87 @@ class Loader
     {
         $controller_name = $controller;
         $action_name = strtolower($action);
-        
+
         // creation of controller
         $controller = ucwords($controller_name);
-	    $controller = new $controller();
-	    
+        $controller = new $controller();
+
         // call action of the controller
-	    if ((int)method_exists($controller_name, $action_name)) {
-		    call_user_func_array(array($controller, $action_name), array_values($url_segments));
-	    } 
+        if((int) method_exists($controller_name, $action_name)) {
+            call_user_func_array(array($controller, $action_name), array_values($url_segments));
+        }
     }
-    
-    /** 
+
+    /**
      * Check if environment is development and display errors 
      */
-    public function set_reporting() 
+    public function set_reporting()
     {
-        if($this->_config->item('development_environment') == true)
-        {
+        if($this->_config->item('development_environment') == true) {
             error_reporting(E_ALL);
-            ini_set('display_errors','On');
+            ini_set('display_errors', 'On');
             ini_set('html_errors', 'On');
-        } 
-        else{
+        }
+        else {
             error_reporting(E_ALL & ~E_DEPRECATED);
-            ini_set('display_errors','Off');
+            ini_set('display_errors', 'Off');
             ini_set('log_errors', 'On');
             ini_set('error_log', ROOT.DS.'system'.DS.'tmp'.DS.'logs'.DS.'error.log');
         }
     }
-    
+
     /**
      * Prepare the framework to the requests
      * @param function $pre_controller a closure to call before controller
      * @param function $post_controller a closure to call after controller
      */
-    public function start($pre_controller = null, $post_controller = null) 
+    public function start($pre_controller = null, $post_controller = null)
     {
         $this->set_reporting();
         $this->_autoload();
-        
+
         // execute a pre-controller call function
-        if(is_callable($pre_controller)) $pre_controller();
-        
-	    // call requested controller
-	    $controller = $this->library('input')->get('controller');
-	    $action = $this->library('input')->get('action');
-	    $this->controller($controller, $action, $this->library('input')->get());
-	    
-	    // execute a post-controller call function
-	    if(is_callable($post_controller)) $post_controller();
-	    
-	    // output all buffer
-	    $this->library('output')->display();
+        if(is_callable($pre_controller))
+            $pre_controller();
+
+        // call requested controller
+        $controller = $this->library('input')->get('controller');
+        $action = $this->library('input')->get('action');
+        $this->controller($controller, $action, $this->library('input')->get());
+
+        // execute a post-controller call function
+        if(is_callable($post_controller))
+            $post_controller();
+
+        // output all buffer
+        $this->library('output')->display();
     }
-    
+
     /**
      * Autoload all classes selected in the config file autoload.php
      */
     private function _autoload()
     {
         // autoload internal libraries
-        foreach($this->_internal_autoload as $lib){
+        foreach($this->_internal_autoload as $lib) {
             $this->library($lib);
         }
-        
+
         // autoload libraries in autoload config file
         $libraries = $this->_config->item('libraries');
-        foreach($libraries as $lib){
+        foreach($libraries as $lib) {
             $this->library($lib);
         }
         unset($libraries, $lib);
-        
+
         // autoload helpers
         $helpers = $this->_config->item('helpers');
-        foreach($helpers as $help){
+        foreach($helpers as $help) {
             $this->helper($help);
         }
         unset($helpers, $help);
     }
+
 }
 
 /* End of file Loader.php */
